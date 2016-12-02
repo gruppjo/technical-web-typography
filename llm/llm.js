3e45887bd5b5b12188c9974f151e515761bea500
gruppjo <jona_blubb@web.de>
2016-12-02 14:08:42 +0100

    add screen dimension measurer

--- llm/llm.js
+++ llm/llm.js
@@ -1,15 +1,29 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-  console.log('dom loaded mofo');
+
+  // llm
   let print = function () {
     console.log('called');
-    llmOut.innerHTML = llmIn.value.length;
+    llmOut.textContent = llmIn.value.length;
   };
   let llmOut = document.getElementById('llm__output');
   let llmIn = document.getElementById('llm__input');
-  debugger;
   llmIn.addEventListener('keyup', print);
-
   print();
 
+  // sdim
+  let sdim = document.getElementById('sdim');
+  let resize = function () {
+    sdim.textContent = window.innerWidth + 'x' + window.innerHeight;
+  }
+  let lastTimeout;
+  window.addEventListener('resize', function () {
+    if (!lastTimeout) {
+      lastTimeout = setTimeout(function () {
+        lastTimeout = null;
+        resize();
+      }, 50);
+    }
+  });
+  resize();
 })
 
